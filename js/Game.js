91fe1e920a0d0ccca7fb3edbf8693cfb746f110e
Kushalkhemka <Kushalkhemka559@gmail.com>
2020-11-22 13:55:16 +0530

    modified

--- js/Game.js
+++ js/Game.js
@@ -23,4 +23,41 @@ class Game {
       form.display();
     }
   }
+   
+  play()
+  {
+    form.hide();
+    stroke("yellow")
+    strokeWeight(1);
+    fill("black")
+    textSize(24)
+    text("GAME START",120,100);
+
+    Player.getplayerinfo();
+
+    if(allPlayers!==undefined)
+    {
+      var display_position=130;
+      for(var plr in allPlayers)
+      {
+        if(plr==="player"+player.index)
+        {
+          fill("red");
+        }
+        else{
+          fill("black");
+        }
+      display_position+=20;
+      textSize(15);
+      text(allPlayers[plr].name+":"+allPlayers[plr].distance,120,display_position);
+      }
+    }
+
+    if(keyIsDown(UP_ARROW)&&player.index!==null)
+    {
+      player.distance+=50;
+      player.update();
+    }
+  }
+   
 }
